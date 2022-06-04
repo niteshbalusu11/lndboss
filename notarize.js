4416f1b2e3e2d202ef91b9f5d8c3a55210944249
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2022-06-04 09:43:48 -0400

    add notary and entitlements

---
+++ notarize.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const electron_notarize = require('electron-notarize');
+
+module.exports = async function (params) {
+  if (process.platform !== 'darwin') {
+    return;
+  }
+
+  console.log('afterSign hook triggered', params);
+
+  const appId = 'com.nitesh.balusu.lndboss';
+
+  const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    console.log('skip');
+    return;
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath} with Apple ID ${process.env.APPLE_ID}`);
+
+  try {
+    await electron_notarize.notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(`Done notarizing ${appId}`);
+};
