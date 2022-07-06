448bb1b692d703b7dfbe97e984b2516746a6e710
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2022-07-06 11:06:44 -0400

    fix session time for dev environment

--- src/server/modules/auth/auth.module.ts
+++ src/server/modules/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { UsersModule } from '../users/users.module';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: !!isProduction ? '15m' : '1m' },
+      signOptions: { expiresIn: !!isProduction ? '15m' : '1h' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
