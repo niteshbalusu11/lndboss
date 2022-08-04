2722eb38f7801a40eec310c432312413c5911913
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2022-08-04 10:04:35 -0400

    consolidate modules and reduce docker image size

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,39 +1,21 @@
 import { WinstonModule, utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import { format, transports } from 'winston';
 
-import { AccountingModule } from './modules/accounting/accounting.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './modules/auth/auth.module';
-import { BalanceModule } from './modules/balance/balance.module';
 import { BosloggerModule } from './modules/boslogger/boslogger.module';
-import { CertValidityDaysModule } from './modules/cert-validity-days/cert-validity-days.module';
-import { ChainDepositModule } from './modules/chain-deposit/chain-deposit.module';
-import { ChainfeesModule } from './modules/chainfees/chainfees.module';
-import { ChartChainFeesModule } from './modules/chart-chain-fees/chart-chain-fees.module';
-import { ChartFeesEarnedModule } from './modules/chart-fees-earned/chart-fees-earned.module';
-import { ChartFeesPaidModule } from './modules/chart-fees-paid/chart-fees-paid.module';
-import { ChartPaymentsReceivedModule } from './modules/chart-payments-received/chart-payments-received.module';
-import { ClosedModule } from './modules/closed/closed.module';
+import { CommandsModule } from './modules/commands/commands.module';
 import { ConfigModule } from '@nestjs/config';
 import { CredentialsModule } from './modules/credentials/credentials.module';
 import { CronModule } from './modules/cron/cron.module';
 import { ExternalServicesModule } from './modules/external-services/external-services.module';
-import { FindModule } from './modules/find/find.module';
-import { ForwardsModule } from './modules/forwards/forwards.module';
-import { GraphModule } from './modules/graph/graph.module';
 import { GrpcModule } from './modules/grpc/grpc.module';
 import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
 import { LndModule } from './modules/lnd/lnd.module';
 import { Module } from '@nestjs/common';
-import { PeersModule } from './modules/peers/peers.module';
-import { PriceModule } from './modules/price/price.module';
-import { ProbeModule } from './modules/probe/probe.module';
 import { RebalanceModule } from './modules/rebalance/rebalance.module';
-import { ReconnectModule } from './modules/reconnect/reconnect.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { SendModule } from './modules/send/send.module';
 import { SocketModule } from './modules/socket/socket.module';
-import { TagsModule } from './modules/tags/tags.module';
 import { ViewModule } from '~server/modules/view/view.module';
 import { homedir } from 'os';
 import { join } from 'path';
@@ -42,23 +24,11 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    AccountingModule,
     AuthModule,
-    BalanceModule,
-    CertValidityDaysModule,
-    ChainDepositModule,
-    ChainfeesModule,
-    ChartChainFeesModule,
-    ChartFeesEarnedModule,
-    ChartFeesPaidModule,
-    ChartPaymentsReceivedModule,
-    ClosedModule,
     CredentialsModule,
+    CommandsModule,
     CronModule,
     ExternalServicesModule,
-    FindModule,
-    ForwardsModule,
-    GraphModule,
     GrpcModule,
     ConfigModule.forRoot({
       isGlobal: true,
@@ -66,15 +36,9 @@ import { join } from 'path';
     }),
     LndModule,
     BosloggerModule,
-    PeersModule,
-    PriceModule,
-    ProbeModule,
     RebalanceModule,
-    ReconnectModule,
     ScheduleModule.forRoot(),
-    SendModule,
     SocketModule,
-    TagsModule,
     ViewModule,
     WinstonModule.forRootAsync({
       useFactory: () => ({
---
+++ src/server/modules/commands/commands.controller.ts
@@ -0,0 +1,123 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import {
+  accountingDto,
+  balanceDto,
+  certValidityDaysDto,
+  chainDepositDto,
+  chainfeesDto,
+  chartChainFeesDto,
+  chartFeesEarnedDto,
+  chartFeesPaidDto,
+  chartPaymentsReceivedDto,
+  closedDto,
+  findDto,
+  forwardsDto,
+  graphDto,
+  peersDto,
+  priceDto,
+  probeDto,
+  reconnectDto,
+  sendDto,
+  tagsDto,
+} from '~shared/commands.dto';
+import { CommandsService } from './commands.service';
+
+@Controller('api')
+export class CommandsController {
+  constructor(private readonly commandsService: CommandsService) {}
+
+  @Get('accounting')
+  async accountingCommand(@Query() args: accountingDto) {
+    return this.commandsService.accountingCommand(args);
+  }
+
+  @Get('balance')
+  async balanceCommand(@Query() args: balanceDto) {
+    return this.commandsService.balanceCommand(args);
+  }
+
+  @Get('cert-validity-days')
+  async certValidityDaysCommand(@Query() args: certValidityDaysDto) {
+    return this.commandsService.certValidityDaysCommand(args);
+  }
+
+  @Get('chain-deposit')
+  async chainDepositCommand(@Query() args: chainDepositDto) {
+    return this.commandsService.chainDepositCommand(args);
+  }
+
+  @Get('chainfees')
+  async chainfeesCommand(@Query() args: chainfeesDto) {
+    return this.commandsService.chainfeesCommand(args);
+  }
+
+  @Get('chart-chain-fees')
+  async chartChainFeesCommand(@Query() args: chartChainFeesDto) {
+    return this.commandsService.chartChainFeesCommand(args);
+  }
+
+  @Get('chart-fees-earned')
+  async chartFeesEarnedCommand(@Query() args: chartFeesEarnedDto) {
+    return this.commandsService.chartFeesEarnedCommand(args);
+  }
+
+  @Get('chart-fees-paid')
+  async chartFeesPaidCommand(@Query() args: chartFeesPaidDto) {
+    return this.commandsService.chartFeesPaidCommand(args);
+  }
+
+  @Get('chart-payments-received')
+  async chartPaymentsReceivedCommand(@Query() args: chartPaymentsReceivedDto) {
+    return this.commandsService.chartPaymentsReceivedCommand(args);
+  }
+
+  @Get('closed')
+  async closedCommand(@Query() args: closedDto) {
+    return this.commandsService.closedCommand(args);
+  }
+
+  @Get('find')
+  async findCommand(@Query() args: findDto) {
+    return this.commandsService.findCommand(args);
+  }
+
+  @Get('forwards')
+  async forwardsCommand(@Query() args: forwardsDto) {
+    return this.commandsService.forwardsCommand(args);
+  }
+
+  @Get('graph')
+  async graphCommand(@Query() args: graphDto) {
+    return this.commandsService.graphCommand(args);
+  }
+
+  @Get('peers')
+  async peersCommand(@Query() args: peersDto) {
+    return this.commandsService.peersCommand(args);
+  }
+
+  @Get('price')
+  async priceCommand(@Query() args: priceDto) {
+    return this.commandsService.priceCommand(args);
+  }
+
+  @Get('probe')
+  async probeCommand(@Query() args: probeDto) {
+    return this.commandsService.probeCommand(args);
+  }
+
+  @Get('reconnect')
+  async reconnectCommand(@Query() args: reconnectDto) {
+    return this.commandsService.reconnectCommand(args);
+  }
+
+  @Get('send')
+  async sendCommand(@Query() args: sendDto) {
+    return this.commandsService.sendCommand(args);
+  }
+
+  @Get('tags')
+  async tagsCommand(@Query() args: tagsDto) {
+    return this.commandsService.tagsCommand(args);
+  }
+}
